7cb2d75d82037ecf8290a00f1e3ca00739557eaf
shadul.shaikh <shaikhshadul66@gmail.com>
2024-12-10 16:46:30 +0530

    test version2

---
+++ first_ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "us-east-1"
+  
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
